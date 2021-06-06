b81fd039a4f62ef36b8d449e65df5b37d33bd315
mi_iroha <chimosy21@gmail.com>
2021-06-06 17:07:53 +0900

    feat: CSVファイルに書き込みを行うロジックを実装

--- AntiAgingDockApp2/app/src/main/java/com/antiaginglab/antiagingdockapp2/ConfirmActivity.kt
+++ AntiAgingDockApp2/app/src/main/java/com/antiaginglab/antiagingdockapp2/ConfirmActivity.kt
@@ -3,9 +3,12 @@ package com.antiaginglab.antiagingdockapp2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+/*
+内部ストレージに保存しているCSVファイルをFirebaseのstorageに登録する
+ */
+class ConfirmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_confirm)
     }
 }
---
+++ AntiAgingDockApp2/app/src/main/java/com/antiaginglab/antiagingdockapp2/InputDataActivity.kt
@@ -0,0 +1,94 @@
+package com.antiaginglab.antiagingdockapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.PrintWriter
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.math.log
+
+/*
+CSVファイルにデータを書き込み、内部ストレージに保存する
+ */
+class InputDataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_data)
+
+        var name = "アンミカ"
+        var height = "171.0"
+        var weight = "58.0"
+
+        val patientsDataList = listOf(name, height, weight)
+
+        // ファイルが存在するか確認
+        val filePath = "/data/data/com.antiaginglab.antiagingdockapp2/files/patients_data.csv"
+        val csvFile = File(filePath)
+        if (csvFile.exists()) {
+            Log.d("TAG1", "ファイルが存在します")
+            addToFile(patientsDataList)
+        } else {
+            Log.d("TAG2", "ファイルが存在しません")
+            createFile(patientsDataList)
+        }
+    }
+
+
+    // ===== ファイルが存在しない場合、ファイルを作成して書き込み =====
+    private fun createFile(patientsDataList: List<String>) {
+        // 出力ファイルの作成
+        val file = File(applicationContext.filesDir, "patients_data.csv")
+        val fw = FileWriter(file, false)
+        val pw = PrintWriter(BufferedWriter(fw))
+
+        // ヘッダーの指定
+        pw.print("名前")
+        pw.print(",")
+        pw.print("身長")
+        pw.print(",")
+        pw.print("体重")
+        pw.println()
+
+        // データを書き込む
+        val listSize = patientsDataList.size
+        for (i in 0 until listSize) {
+            if(i == listSize-1) {
+                pw.print(patientsDataList[listSize - 1])
+                pw.println()
+            } else {
+                pw.print(patientsDataList[i])
+                pw.print(",")
+            }
+        }
+
+        // ファイルを閉じる
+        pw.close()
+    }
+
+    // ===== ファイルが存在する場合、ファイルに追記 =====
+    private fun addToFile(patientsDataList: List<String>) {
+        // 出力ファイルの作成
+        val file = File(applicationContext.filesDir, "patients_data.csv")
+        val fw = FileWriter(file, true)
+        val pw = PrintWriter(BufferedWriter(fw))
+
+        // データを書き込む
+        val listSize = patientsDataList.size
+        for (i in 0 until listSize) {
+            if(i == listSize-1) {
+                pw.print(patientsDataList[listSize - 1])
+                pw.println()
+            } else {
+                pw.print(patientsDataList[i])
+                pw.print(",")
+            }
+        }
+
+        // ファイルを閉じる
+        pw.close()
+    }
+}
