92ee8cbfb21517017267a0e93b25ac524263ce77
mi_iroha <chimosy21@gmail.com>
2021-06-08 11:06:49 +0900

    fix: firebase storageにファイルが保存されない問題を解決した

--- AntiAgingDockApp2/app/src/main/java/com/antiaginglab/antiagingdockapp2/ConfirmRepository.kt
+++ AntiAgingDockApp2/app/src/main/java/com/antiaginglab/antiagingdockapp2/ConfirmRepository.kt
@@ -5,9 +5,10 @@ import android.util.Log
 import com.google.android.gms.tasks.Task
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
+import kotlinx.coroutines.suspendCancellableCoroutine
 import java.io.File
-import java.io.FileInputStream
 import kotlin.coroutines.suspendCoroutine
+
 
 class ConfirmRepository {
     private val storage = Firebase.storage
@@ -16,13 +17,6 @@ class ConfirmRepository {
         return suspendCoroutine {
 
             val storageRef = storage.reference
-
-//            val ref = storageRef.child("patients_data.csv")
-//            val stream = FileInputStream(csvFile)
-//            val uploadTask = ref.putStream(stream)
-//            uploadTask.addOnSuccessListener { taskSnapshot ->
-//                Log.d("SUCCESS", taskSnapshot.toString())
-//            }
 
             val file = Uri.fromFile(csvFile)
             val ref = storageRef.child("files/${file.lastPathSegment}")
--- AntiAgingDockApp2/app/src/main/java/com/antiaginglab/antiagingdockapp2/InputDataActivity.kt
+++ AntiAgingDockApp2/app/src/main/java/com/antiaginglab/antiagingdockapp2/InputDataActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.util.CollectionUtils.listOf
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
@@ -96,3 +97,7 @@ class InputDataActivity : AppCompatActivity() {
         pw.close()
     }
 }
+
+
+
+
