2e543d7b9dc2021c8b5f9aa0985e55e9d4d5c021
mi_iroha <chimosy21@gmail.com>
2021-07-11 21:43:59 +0900

    feat: InputDataActivity1にカスタムツールバーを設置

--- AntiAgingDockApp2/app/src/main/java/com/antiaginglab/antiagingdockapp2/InputDataActivity1.kt
+++ AntiAgingDockApp2/app/src/main/java/com/antiaginglab/antiagingdockapp2/InputDataActivity1.kt
@@ -1,10 +1,15 @@
 package com.antiaginglab.antiagingdockapp2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.PopupMenu
 import com.antiaginglab.antiagingdockapp2.databinding.ActivityInputData1Binding
 
-class InputDataActivity1 : AppCompatActivity() {
+class InputDataActivity1 : AppCompatActivity(), ToolBarCustomViewDelegate {
 
     private lateinit var binding: ActivityInputData1Binding
 
@@ -14,6 +19,34 @@ class InputDataActivity1 : AppCompatActivity() {
         binding = ActivityInputData1Binding.inflate(layoutInflater)
                 .apply { setContentView(this.root) }
 
+        // デフォルトのアクションバーを非表示にする
+        supportActionBar?.hide()
         
+        // カスタムツールバーの設置
+        setCustomToolBar()
+
     }
+
+    // ============= ツールバーの設定 =============
+    override fun onClickedLeftButton(){ }
+    override fun onClickedRightButton(){ }
+
+    private fun setCustomToolBar() {
+        val toolBarCustomView = ToolBarCustomView(this)
+        toolBarCustomView.delegate = this
+
+        val title = getString(R.string.title_tool_bar)
+        toolBarCustomView.configure(title, true, false)
+
+        // カスタムツールバーを挿入するコンテナ(入れ物)を指定
+        val layout: LinearLayout = binding.containerForToolBar
+        // ツールバーの表示をコンテナに合わせる
+        toolBarCustomView.layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        // カスタムツールバーを表示する
+        layout.addView(toolBarCustomView)
+    }
+
 }
